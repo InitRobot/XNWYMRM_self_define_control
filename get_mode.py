7e6ca2cf901a0b7df80e3d6c658b9f301000ef1d
victorchenjiantian <vict2008or0411@163.com>
2024-01-19 17:13:53 +0800

    放弃

--- get_mode.py
+++ get_mode.py
@@ -7,14 +7,28 @@ import sys
 # 组网模式下，机器人当前 IP 地址为 192.168.1.176, 控制命令端口号为 40923
 # 机器人 IP 地址根据实际 IP 进行修改
 host = "192.168.1.176"
-port = 40923
+port_TCP = 40923
+port_UDP = 40924
 
-def connect():
+def connect_TCP():
         global s
-        address = (host, int(port))
+        address = (host, int(port_TCP))
 
         # 与机器人控制命令端口建立 TCP 连接
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        print("Connecting...")
+
+        s.connect(address)
+
+        print("Connected!")
+
+def connect_UDP():
+        global s
+        address = (host, int(port_UDP))
+
+        # 与机器人控制命令端口建立 UDP 连接
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         print("Connecting...")
 
@@ -30,23 +44,24 @@ def disconnect():
 
 def get_msg():
         global s
-        msg = "robot battery ?;"
+        msg = "chassis attitude ?;"
+        print(msg.encode('utf-8'))
         a = s.send(msg.encode('utf-8'))
-        print(a)
-        print('send')
-        print("msg")
+        return (a)
+        #print('send')
+        #print("msg")
 
 if __name__ == '__main__':
-        connect()
+        connect_TCP()
         i = 0
-        print('1')
+        #print('1')
         msg = "commend;"
         s.send(msg.encode('utf-8'))
-        while (i <= 1000):
+        while (i <= 9):
                 i += 1
-                print('s')
-                get_msg()
-                print(i)
+                #print('s')
+                print(i,':',get_msg())
+                #print(i)
                 if ():
                         print('break')
                         break
