a58f9d0a60841ce5c9af8bd8646a7306456f3feb
victorchenjiantian <vict2008or0411@163.com>
2024-02-17 16:43:21 +0800

    实现自动第一步，通过函数判断xy预期值，并通过PID减小误差，还需调参

--- SDK_get_msg/auto_time_move.py
+++ SDK_get_msg/auto_time_move.py
@@ -3,8 +3,8 @@ import Message_Delivery
 import MSG_Solve
 import Chassis_Solve
 import Chassis_Move
+import time
 #import matplotlib.pyplot as plt
-
 
 kp = 0.5
 target = 0.5
@@ -15,20 +15,26 @@ Message_Delivery.connect_UDP(printing=False)
 SDK_.IN_OUT("game_msg on;",printing=False)
 SDK_.IN_OUT("chassis push position on pfreq 50;",printing=False)
 SDK_.IN_OUT("robot mode free;",printing=False)
-for i in range(1,200):
-    #print(i)
+
+start_time = time.perf_counter()
+
+now_time = time.perf_counter() - start_time
+
+for i in range(1,150):
+    print(i)
+    now_time = time.perf_counter() - start_time
+    x_target = (0.25 * (now_time - 2) ** 2 - 1)*5
     msg = Message_Delivery.try_get(timeout = 1,printing=False)
-    print(msg)
     chassis_position = []
     chassis_position = MSG_Solve.solve_chassis_position(msg,printing=False)
-    #print(chassis_position)
+    print(chassis_position)
     #chassis speed x 0.1 y 0.1 z 1;
     if chassis_position != []:
-        error = target - chassis_position[0]
+        error = x_target - chassis_position[0]
         error_list.append(error)
         x_speed = kp * error
-        #print("--------------",x_speed)
-        #SDK_.IN_OUT("chassis speed x " + str(x_speed) + " y 0 z 0;",printing=True)
-#print(error_list)
+        print("--------------",x_speed)
+        SDK_.IN_OUT("chassis speed x " + str(x_speed) + " y 0 z 0;",printing=True)
+print(error_list)
 #plt.plot(range(1,200),error_list)
 #plt.show()
